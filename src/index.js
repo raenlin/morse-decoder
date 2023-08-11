b8f63fa387849628374b30e33dde5fe3227ecf13
Darya Pchelintseva <raelin@mail.ru>
2023-08-11 23:11:17 +0300

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,47 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(str) {
+    let arr = str.split('');
+
+let arrSliced = [];
+
+while (arr.length > 0 ) {
+  const sliced = arr.splice(0, 10).join('');
+  arrSliced.push(sliced);
+}
+
+let arrReplaced = [];
+for(let item of arrSliced) {
+  let elem = '10';
+  while(item.includes(elem)) {
+    item = item.replace(elem, '.');
+  }
+  arrReplaced.push(item);
+}
+
+let arrReplaced2 = []; 
+for(let item of arrReplaced) {
+  let elem = '11';
+  while(item.includes(elem)) {
+    item = item.replace(elem, '-');
+  }
+  arrReplaced2.push(item);
+}
+
+for(let i=0; i < arrReplaced2.length; i++) {
+  arrReplaced2[i] = arrReplaced2[i].replace(/\d/g, '');
+}
+
+let result = '';
+
+for(let elem of arrReplaced2) {
+  result = result + MORSE_TABLE[elem];
+}
+return result;
 }
 
 module.exports = {
